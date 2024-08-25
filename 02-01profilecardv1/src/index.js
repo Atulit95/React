61718fce649adc1fa024ca7c6e3739dfa9e4f998
Atulit95 <atulitgupta436@gmail.com>
2024-08-25 11:39:33 +0530

    profilecardv1

--- 02-01profilecardv1/src/index.js
+++ 02-01profilecardv1/src/index.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function App() {
   return (
-    <div className='card'>
+    <div className="card">
       <Avatar />
-      <div className='data'>
-        <h1>Hello</h1>
+      <div className="data">
+        <Intro />
+        <SkillList />
       </div>
     </div>
-  )
+  );
 }
 
-function Avatar(){
+function Avatar() {
   return (
     <div>
-      <img src="images/Avatar.png" className='image'></img>
+      <img src="images/Avatar.png" className="image"></img>
     </div>
-  )
+  );
+}
+
+function Intro() {
+  return (
+    <>
+      <h1>Jonas Schmedtmann</h1>
+      <p>
+        Full-Stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook(and eat), or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+    </>
+  );
+}
+
+function SkillList(props) {
+  return (
+    <>
+      <div className="skill-container">
+      <Skill skill="Html+Css" emoji="💪" color="grey"/>
+      <Skill skill="React" emoji="💪" color="orange"/>
+      <Skill skill="Javascript" emoji="💪" color="yellow"/>
+      <Skill skill="Svelte" emoji="😒" color="red"/>
+      </div>
+    </>
+  );
+}
+
+function Skill(props){
+  return(
+    <div className="skill" style={{backgroundColor: props.color}}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
 }
 
 root.render(
@@ -32,4 +68,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
