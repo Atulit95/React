ad0fbcf7698890f5b9b3c21c3893efe8f649cb47
Atulit <atulitgupta436@gmail.com>
2024-09-19 07:45:02 +0530

    code updated

--- 04_01far-away/src/App.js
+++ 04_01far-away/src/App.js
@@ -109,6 +109,7 @@ function PackingList({ items, ondeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed Status</option>
         </select>
+        <button>Clear list</button>
       </div>
     </div>
   );
