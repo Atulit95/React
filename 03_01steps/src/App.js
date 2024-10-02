735e7f6bf4f344d22677ce0677c7eb13b29bbea0
Atulit <atulitgupta436@gmail.com>
2024-10-02 18:06:43 +0530

    updated

--- 03_01steps/src/App.js
+++ 03_01steps/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Children, useState } from "react";
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -25,7 +25,7 @@ export default function App() {
       <button
         className="close"
         onClick={() => {
-          setIsOpen(is => !isOpen);
+          setIsOpen((is) => !isOpen);
         }}
       >
         &times;
@@ -37,25 +37,43 @@ export default function App() {
             <div className={`${step >= 2 ? "active" : ""}`}>2</div>
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span>Previous
+            </Button>
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              text="Next"
               onClick={handelNext}
+              emoji=""
             >
-              Next
-            </button>
+              Next<span>👉</span>
+            </Button>
           </div>
         </div>
       ) : null}
     </>
+  );
+}
+
+function StepMessage({ children, step }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
