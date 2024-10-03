89b547dfecff81cca61454cf4ad0ecdfbcb471ae
Atulit <atulitgupta436@gmail.com>
2024-10-03 22:03:15 +0530

    Accordian v2

---
+++ 04_04Accordian Component Challenge/src/Accordionv2.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Accordion({ data }) {
+  const [curOpen, setCuropen] = useState(null);
+
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCuropen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >{el.text}</AccordionItem>
+      ))}  
+    </div>
+  );
+}
+
+function AccordionItem({ num, title,curOpen, onOpen,children }) {
+  const isOpen = num === curOpen
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{children}</div>}
+    </div>
+  );
+}
